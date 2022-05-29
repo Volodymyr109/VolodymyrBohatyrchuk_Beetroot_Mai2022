d047c5e4bd3aceeb7f7d40f8c8a85a04342b3133
Volodymyr Bohatyrchuk <100191534+Volodymyr109@users.noreply.github.com>
2022-05-29 17:20:57 +0300

    Add files via upload

---
+++ beetrood_homework_2.py
@@ -0,0 +1,34 @@
+"""try to lay here comment"""
+# TASK_1 - greeting program and
+# TASK_2 The greeting program and strings concatenation  with name.
+
+
+hi= "Hallo,"
+name_1= "Volodymyr"
+name_2= "Bohatyrchuk"
+day= "\nSunday " ""
+py= "is beautiful day to learn some Python"
+End= "\nBeetroot"
+print(hi[0:6])
+print(name_1 + " " + name_2 + day + py)
+print(End[1]+ ".")
+
+
+#Task_3
+"""Addition
+Subtraction
+Division
+Multiplication
+Exponent (Power)
+Modulus
+Floor division"""
+
+a=100
+b=7
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)
+print(a//b)
